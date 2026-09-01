75ee598635f10387860e38aecd13237dce35960f
mainhoonkon666-cloud <mainhoonkon666@gmail.com>
2026-09-01 22:58:12 +0530

    Time: 1 ms (95%), Space: 44 MB (68.66%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 4034-minimum-bishop-moves-to-reach-target/4034-minimum-bishop-moves-to-reach-target.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minBishopMoves(int[] source, int[] target) {
+
+        int sr = source[0];
+        int sc = source[1];
+
+        int tr = target[0];
+        int tc = target[1];
+
+        if(sr == tr && sc == tc){
+            return 0;
+        }
+
+        if((sr + sc) % 2 != (tr + tc) % 2){
+            return -1;
+        }
+
+        if(Math.abs(sr - tr) == Math.abs(sc - tc)){
+            return 1;
+        }
+        return 2;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
