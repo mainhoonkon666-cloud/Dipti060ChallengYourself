4fc873c1cdd8aba5a5cbc4585e74b2703e767387
mainhoonkon666-cloud <mainhoonkon666@gmail.com>
2026-09-04 18:32:57 +0530

    Time: 2 ms (99.95%), Space: 52.4 MB (65.21%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -0,0 +1,63 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+
+        int candidate1 = 0;
+        int candidate2 = 0;
+
+        int count1 = 0;
+        int count2 = 0;
+
+        // Step 1: Find possible candidates
+        for (int num : nums) {
+
+            if (num == candidate1) {
+                count1++;
+            }
+            else if (num == candidate2) {
+                count2++;
+            }
+            else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            }
+            else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            }
+            else {
+                count1--;
+                count2--;
+            }
+        }
+
+        // Step 2: Verify candidates
+        count1 = 0;
+        count2 = 0;
+
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            }
+            else if (num == candidate2) {
+                count2++;
+            }
+        }
+
+        // Step 3: Add valid candidates
+        List<Integer> result = new ArrayList<>();
+
+        if (count1 > nums.length / 3) {
+            result.add(candidate1);
+        }
+
+        if (count2 > nums.length / 3) {
+            result.add(candidate2);
+        }
+
+        return result;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
