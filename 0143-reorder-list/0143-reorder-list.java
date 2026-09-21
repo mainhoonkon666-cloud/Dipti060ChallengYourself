e5327e0ff0c69339c954f92739e28eb80210f4eb
mainhoonkon666-cloud <mainhoonkon666@gmail.com>
2026-09-21 23:27:09 +0530

    Time: 2 ms (86.45%), Space: 49.4 MB (36.73%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0143-reorder-list/0143-reorder-list.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode mid = slow;
+
+        ListNode curr = mid.next;
+        mid.next = null;
+        ListNode prev = null;
+        ListNode next;
+
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        ListNode left = head;
+        ListNode right = prev;
+        ListNode nextL, nextR;
+
+        while(left != null && right != null){
+            nextL = left.next;
+            left.next = right;
+            nextR = right.next;
+            right.next = nextL;
+
+            left = nextL;
+            right = nextR;
+        }
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
