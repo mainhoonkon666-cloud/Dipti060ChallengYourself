99d27283fb8892b34826f4b7b5ee8621f8f0870e
mainhoonkon666-cloud <mainhoonkon666@gmail.com>
2026-09-22 23:49:07 +0530

    Time: 0 ms (100%), Space: 43.2 MB (29.39%) - LeetHub - Time: O(N), Space: O(1)

---
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode prev = dummy;
+
+        // Move prev to the node before left
+        for (int i = 1; i < left; i++) {
+            prev = prev.next;
+        }
+
+        ListNode curr = prev.next;
+
+        // Reverse from left to right
+        for (int i = 0; i < right - left; i++) {
+
+            ListNode next = curr.next;
+
+            curr.next = next.next;
+
+            next.next = prev.next;
+
+            prev.next = next;
+        }
+
+        return dummy.next;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
